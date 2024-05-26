d78a0d9d4c5b13bbadd39e73b6765f5d59ba419d
bbarren <bbarren@ncsu.edu>
2024-05-26 10:02:37 -0400

    made changes to enable the program to be run from the desktop for ease of use

--- usgsFlightInfo/FlightInfo.cpp
+++ usgsFlightInfo/FlightInfo.cpp
@@ -3,10 +3,34 @@
 #include <fstream>
 #include <vector>
 #include <limits>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 #include "Flight/Flight.h"
 
 using namespace std;
+
+string getFullPath(const string &relativePath) {
+    const char* homeDir = getenv("HOME");
+    if (homeDir == nullptr) {
+        cerr << "Error: HOME environment variable not set." << endl;
+        return "";
+    }
+
+    string fullPath = string(homeDir) + "/" + relativePath;
+
+    // Check if the directory exists
+    size_t pos = fullPath.find_last_of('/');
+    if (pos != string::npos) {
+        string dir = fullPath.substr(0, pos);
+        struct stat info;
+        if (stat(dir.c_str(), &info) != 0 || !(info.st_mode & S_IFDIR)) {
+            cerr << "Error: Directory " << dir << " does not exist." << endl;
+            return "";
+        }
+    }
+    return fullPath;
+}
 
 int main() {
     char day;
@@ -16,12 +40,23 @@ int main() {
     cout << "Please enter the day(S/M/T/F): ";
     cin >> day;
     day = toupper(day);
+    string fullPath;
     if (day == 'S' || day == 'T') {
         cout << "\nOpening Sunday/Thursday schedule...\n";
-        file.open("schedules/sun-thurs schedule.txt");
+        fullPath = getFullPath("Desktop/schedules/sun-thurs schedule.txt");
+        if (fullPath.empty()) {
+            cerr << "Unable to resolve full path for the schedule file." << endl;
+            return -1;
+        }
+        file.open(fullPath);
     } else if (day == 'M' || day == 'F') {
         cout << "\nOpening Monday/Friday schedule...\n";
-        file.open("schedules/mon-fri schedule.txt");
+        fullPath = getFullPath("Desktop/schedules/mon-fri schedule.txt");
+        if (fullPath.empty()) {
+            cerr << "Unable to resolve full path for the schedule file." << endl;
+            return -1;
+        }
+        file.open(fullPath);
     } else {
         cerr << "Invalid day. Please try again." << endl;
         return -1;
@@ -97,18 +132,22 @@ int main() {
         return -1;
     }
 
-    remove("output.txt");
-    ofstream output;
-    output.open("output.txt");
+    string outputPath = getFullPath("Desktop/output.txt");
+    if (outputPath.empty()) {
+        cerr << "Unable to resolve full path for output file." << endl;
+        return -1;
+    }
+
+    remove(outputPath.c_str());
+    ofstream output(outputPath.c_str());
     if (output.is_open()) {
         for (Flight flight : flights) {
             output << flight.printFlight();
         }
         output.close();
-        cout << "Output has been saved to output.txt" << endl;
-
+        cout << "Output has been saved to output.txt at path: " << outputPath << endl;
     } else {
-        cerr << "unable to open output file" << endl;
+        cerr << "unable to open output file at path: " << outputPath << endl;
         return -1;
     }
 
